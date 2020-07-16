9c9fc390c1636753785220a3269157e1f43ec0b9
YounghwanYoon <ray.yoon87@gmail.com>
2020-07-16 02:37:59 -0400

    Starting point where did not apply any of design pattern
    This contains:
    - No design pattern / Architecture
    - Retrofit to getting data from server

--- app/src/main/java/com/ray/demo/git/repository/data/GithubResponse.kt
+++ app/src/main/java/com/ray/demo/git/repository/data/GithubResponse.kt
@@ -13,3 +13,32 @@ data class GithubResponse(
     val `private`: Boolean,
     val owner: Owner
 )
+
+/*
+{
+    "id": 26,
+    "node_id": "MDEwOlJlcG9zaXRvcnkyNg==",
+    "name": "merb-core",
+    "full_name": "wycats/merb-core",
+    "private": false,
+    "owner": {
+      "login": "wycats",
+      "id": 4,
+      "node_id": "MDQ6VXNlcjQ=",
+      "avatar_url": "https://avatars0.githubusercontent.com/u/4?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/wycats",
+      "html_url": "https://github.com/wycats",
+      "followers_url": "https://api.github.com/users/wycats/followers",
+      "following_url": "https://api.github.com/users/wycats/following{/other_user}",
+      "gists_url": "https://api.github.com/users/wycats/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/wycats/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/wycats/subscriptions",
+      "organizations_url": "https://api.github.com/users/wycats/orgs",
+      "repos_url": "https://api.github.com/users/wycats/repos",
+      "events_url": "https://api.github.com/users/wycats/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/wycats/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+ */
